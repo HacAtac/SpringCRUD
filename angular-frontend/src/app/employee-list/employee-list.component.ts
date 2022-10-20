a9184ef8156df37407ecf9f72805595b22265b19
Jordan Hackworth <jhackworth00@outlook.com>
2022-10-20 00:25:06 -0500

    set up employee class, then used dummy data to show the employeeList class

---
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css'],
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.employees = [
+      {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        emailId: 'johndoe@gmail.com',
+      },
+      {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        emailId: 'janedoe@gmail.com',
+      },
+    ];
+  }
+}
